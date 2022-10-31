d13602279d512a65ac6dc074e5bcf367ac35c684
Tháinno Santana <94317503+Thainno@users.noreply.github.com>
2022-10-30 22:24:41 -0300

    Correção letra i

--- slideshow.js
+++ slideshow.js
@@ -1,11 +1,11 @@
 'use strict';
 
 const images = [
-    {'id': '1', 'url': '../images/arara.jpg'},
-    {'id': '2', 'url': '../images/onca.jpg'},
-    {'id': '3', 'url': '../images/tucano.jpg'},
-    {'id': '4', 'url': '../images/boto.jpg'},
-    {'id': '5', 'url': '../images/mico-leao.jpg'},
+    {'id': '1', 'url': '../Images/arara.jpg'},
+    {'id': '2', 'url': '../Images/onca.jpg'},
+    {'id': '3', 'url': '../Images/tucano.jpg'},
+    {'id': '4', 'url': '../Images/boto.jpg'},
+    {'id': '5', 'url': '../Images/mico-leao.jpg'},
 ]
 
 const containerItens = document.querySelector('#container-itens');
